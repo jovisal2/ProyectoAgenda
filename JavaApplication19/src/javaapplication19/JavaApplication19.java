fe226f1314906cb7d9d47b9c4283d41d1f1a6b26
jovisal2 <47533597+jovisal2@users.noreply.github.com>
2019-02-11 19:16:51 +0100

    Update JavaApplication19.java

--- JavaApplication19/src/javaapplication19/JavaApplication19.java
+++ JavaApplication19/src/javaapplication19/JavaApplication19.java
@@ -15,7 +15,7 @@ public class JavaApplication19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Cambio en el comentario
+        // Cambio en el comentario modificado desde hitgub.com
     }
     
 }
