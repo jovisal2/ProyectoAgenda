84e72d28e203e22b3973d39c44f30cd482396957
informaticacicles <informaticacicles@gmail.com>
2019-02-12 15:54:54 +0100

    Proyecto Agenda

---
+++ ProyectoAgenda/src/org/simarro/dam/Agenda.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.simarro.dam;
+
+/**
+ *
+ * @author lliurex
+ */
+public class Agenda {
+    
+    int anyo;
+
+    public Agenda() {
+    }
+
+    public Agenda(int anyo) {
+        this.anyo = anyo;
+    }
+
+    public int getAnyo() {
+        return anyo;
+    }
+
+    public void setAnyo(int anyo) {
+        this.anyo = anyo;
+    }
+    
+}
